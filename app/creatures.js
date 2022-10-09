30f30f5f28c2ac986ce824d9cbe6e0fae97b7680
Ajonye Paul <ajonyechase@gmail.com>
2022-10-09 15:27:59 +0100

    add additional creatures

--- app/creatures.js
+++ app/creatures.js
@@ -53,4 +53,57 @@ const creatures = [
 		about:
 			"Bisaan were said to have originated in the forests of Malaysia and are very rare in America. In their true forms they have no genitalia as a result they emerge from underground every 27 years and possess humans through the mouth, so they can mate and breed.  ",
 	},
+	{
+		name: "Changelings",
+		image: "/img/changelings.jpg",
+		about:
+			"Changelings are humanoid creatures with discolored, slimy skin, hollow eyes, and a round mouth with many teeth. They can appear human, but their true nature is revealed in reflection. They feed on humans, specifically on synovial fluid.",
+	},
+	{
+		name: "Cherubim",
+		image: "/img/cherubim.jpg",
+		about:
+			"Cherubs, also called Cupids by humans, are a type of angel that seem to be at the very bottom of Heaven's hierarchy.They can manipulate the fosterence of love, and are often docilistic and pacifistic, but do wield angel blades. They are happy-go-lucky, but easily saddened. ",
+	},
+	{
+		name: "Crocottas",
+		image: "/img/crocottas.jpg",
+		about: "Crocottas are creatures that can mimic any person's voice to lure their victims out so they can feed.",
+	},
+	{
+		name: "Cross-road demon",
+		image: "/img/crossroads_demons.jpg",
+		about:
+			"Crossroad Demons are demons tasked with 'buying' souls for Hell through deals with humans. These deals consist of the demon granting the human's wish in exchange for ownership over that person's soul, resulting in the person dying and going to Hell to be transformed into a demon upon death.",
+	},
+	{
+		name: "Deava",
+		image: "/img/deava.jpg",
+		about:
+			"A Daeva is an ancient demonic creature of shadow, a special type of demon, that can be summoned by other demons. Similar to hellhounds and unlike most demons, these demons do not need a host to attack humans. ",
+	},
+	{
+		name: "Deities",
+		image: "/img/deities.jpg",
+		about:
+			"Deities better known simply as Gods or Pagan/Old Gods, are a race of immortal supernatural entities, with each being a prime authority in the religion or pantheon that they established or belonged to. Despite having a role in the human ancient religions and possessing free will, the plurality of them are cruel, petty, uncaring, and only concerned with themselves and their survival. ",
+	},
+	{
+		name: "Demon",
+		image: "/img/demon.jpg",
+		about:
+			"Demons are malevolent spiritual entities that are the direct opposites to angels. They are created from human souls that have endured extensive torture in Hell by Alastair and other demons. In this process, they become corrupted, extremely evil, and also very powerful.",
+	},
+	{
+		name: "Eve",
+		image: "/img/eve.jpg",
+		about:
+			"Eve was a mysterious and powerful being known by the title of Mother of All. She was in the depths of Purgatory until she was released by dragons and arrived on Earth using a young virgin girl as a vessel. Eve created the Alphas, which were the first monsters of their allotted kind from whom their entire species is descended",
+	},
+	{
+		name: "Fiaries",
+		image: "/img/fiaries.jpg",
+		about:
+			"Fairies are mischievous supernatural beings that come in a variety of forms. They can only be seen by those who have been to their realm or if they choose to be seen.  It is revealed that they are the cause of UFO sightings and other 'extraterrestrial' encounters around Earth.",
+	},
 ];
